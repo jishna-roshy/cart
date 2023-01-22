7dea012f8d81067b8cd865b5341081047690cbc8
jishna-roshy <jishnaroshy9865@gmail.com>
2023-01-23 01:55:22 +0530

    ecommerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
